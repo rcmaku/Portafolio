f069b910a52ccb050bcc4feb630e3283522f55b2
rcmaku <joseprado2293@gmail.com>
2023-11-04 00:59:17 -0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import * as glob from 'glob';
+import path, {resolve} from 'node:path';
+import {ViteMinifyPlugin} from 'vite-plugin-minify'
+import htmlPurge from 'vite-plugin-purgecss'
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig({
+    build:{
+        rollupOptions: {
+            input: Object.fromEntries(
+                [...glob.sync('./(dist)/*.html').map(file=> [
+                    file.slice(0,file.length - path.extname(file).length),resolve(__dirname,file)
+                ]),
+                ...glob.sync('./*.html').map(file=>[
+                    file.slice(0,file.length - path.extname(file).length),resolve(__dirname,file)
+                ])]
+            ),
+        },
+    },
+    plugins:[
+        handlebars({
+            partialDirectory: resolve(__dirname,'partials'),
+        }),
+        htmlPurge({}),
+        ViteMinifyPlugin({}),
+    ]
+})
